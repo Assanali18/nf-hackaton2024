b3e722f37643a262106701c45491f697c55be589
sharn1999 <58104696+sharn1999@users.noreply.github.com>
2024-07-11 18:22:31 +0600

    addOpenai

--- backend/src/AIFunction.ts
+++ backend/src/AIFunction.ts
@@ -12,24 +12,23 @@ async function AIFunction(link, nameOfList) {
     
     //readTable
 
-    await readSheetData(spreadsheetId, range)
-    .then(data => saveDataToJsonFile(data))
-    .catch(error => console.error('Error:', error));
+    // await readSheetData(spreadsheetId, range)
+    // .then(data => saveDataToJsonFile(data))
+    // .catch(error => console.error('Error:', error));
 
 
     //update json
 
     const oldData = readJsonFile('output.json');
     const promtData = await generateFullText(oldData);
-    console.log(promtData);
     
     // await saveDataToJsonFile(promtData)
 
 
     //update table
 
-    const newData = readJsonFile('output.json')
-    await updateSheetData(spreadsheetId, range, newData)
+    // const newData = readJsonFile('output.json')
+    // await updateSheetData(spreadsheetId, range, newData)
 }
 
 export {AIFunction}
--- backend/src/promts.ts
+++ backend/src/promts.ts
@@ -1,23 +1,32 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
+// import { GoogleGenerativeAI } from "@google/generative-ai";
 import dotenv from 'dotenv';
+
+
+
+// const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY as string);
+// const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", generationConfig: { "responseMimeType": "application/json" }});
+import OpenAI from "openai";
 
 dotenv.config();
 
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY as string);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", generationConfig: { "responseMimeType": "application/json" }});
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI,
+  });
 
 async function generateFullText(data){
     const promptFullText = `Give me top 20 the best film of the history`;
 
   try{
+    const response = await openai.chat.completions.create({
+        model: "gpt-4",
+        messages: [
+          { role: "user", content: promptFullText }
+        ]
+      });
 
+    console.log(response.choices[0].message.content);
 
-    const promptResult = await model.generateContent(promptFullText);
-    const text = promptResult.response.text();
-    
-    const jsonResult = JSON.parse(text);
-
-    return jsonResult
+    return response.choices[0].message.content
   } catch(error){    
     console.log(error);
     return null
